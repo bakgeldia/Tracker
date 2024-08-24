c2a747e06bd4dec53dc0b64bfb4c07271e39ce61
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-08-24 20:34:56 +0500

    - SearchBar и заголовок экрана Трекеры помещены в NavBar - Добавлен CollectionView

--- Tracker/Trackers/TrackerCollectionViewCell.swift
+++ Tracker/Trackers/TrackerCollectionViewCell.swift
@@ -8,18 +8,121 @@
 import UIKit
 
 final class TrackerCollectionViewCell: UICollectionViewCell {
-    let titleLabel: UILabel = UILabel()
+    let emojiAndNameView = UIView()
+    let emoji = UILabel()
+    let trackerName = UILabel()
+    
+    let daysAndButtonView = UIView()
+    let numOfDays = UILabel()
+    let completeTrackerButton = UIButton()
+    
+    let trackerView = UIView()
+    
+    var checkedButton = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        contentView.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        setupCell()
+    }
+    
+    private func setupCell() {
+        //Tracker View
+        contentView.addSubview(trackerView)
+        trackerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Emoji and tracker name view rgba(51, 207, 105, 1)
+        emojiAndNameView.backgroundColor = UIColor(red: 51.0/255.0, green: 207.0/255.0, blue: 105.0/255.0, alpha: 1)
+        emojiAndNameView.layer.cornerRadius = 16
+        trackerView.addSubview(emojiAndNameView)
+        emojiAndNameView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        //Emoji
+        emojiAndNameView.addSubview(emoji)
+        emoji.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Tracker name
+        //trackerName.text = "Поливать растения"
+        trackerName.textColor = .white
+        trackerName.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        emojiAndNameView.addSubview(trackerName)
+        trackerName.translatesAutoresizingMaskIntoConstraints = false
+        //trackerName.numberOfLines = 0
+        
+        //Num of days and button
+        trackerView.addSubview(daysAndButtonView)
+        daysAndButtonView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Days label
+        numOfDays.text = "1 day"
+        numOfDays.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        daysAndButtonView.addSubview(numOfDays)
+        numOfDays.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Button
+        completeTrackerButton.setTitle("+", for: .normal)
+        completeTrackerButton.titleLabel?.textColor = .white
+        completeTrackerButton.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .medium)
+        completeTrackerButton.backgroundColor = UIColor(red: 51.0/255.0, green: 207.0/255.0, blue: 105.0/255.0, alpha: 1)
+        completeTrackerButton.layer.cornerRadius = 22
+        completeTrackerButton.addTarget(self, action: #selector(self.didTapCompleteButton), for: .touchUpInside)
+        daysAndButtonView.addSubview(completeTrackerButton)
+        completeTrackerButton.translatesAutoresizingMaskIntoConstraints = false
+        
         
         NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            trackerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            trackerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            trackerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            trackerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            emojiAndNameView.topAnchor.constraint(equalTo: trackerView.topAnchor),
+            emojiAndNameView.leadingAnchor.constraint(equalTo: trackerView.leadingAnchor),
+            emojiAndNameView.trailingAnchor.constraint(equalTo: trackerView.trailingAnchor),
+            emojiAndNameView.heightAnchor.constraint(equalToConstant: 90),
+            
+            emoji.heightAnchor.constraint(equalToConstant: 24),
+            emoji.widthAnchor.constraint(equalToConstant: 24),
+            emoji.topAnchor.constraint(equalTo: emojiAndNameView.topAnchor, constant: 12),
+            emoji.leadingAnchor.constraint(equalTo: emojiAndNameView.leadingAnchor, constant: 12),
+            
+            trackerName.topAnchor.constraint(equalTo: emoji.bottomAnchor, constant: 8),
+            trackerName.leadingAnchor.constraint(equalTo: emojiAndNameView.leadingAnchor, constant: 12),
+            trackerName.trailingAnchor.constraint(equalTo: emojiAndNameView.trailingAnchor, constant: -12),
+            trackerName.bottomAnchor.constraint(equalTo: emojiAndNameView.bottomAnchor, constant: -12),
+
+            daysAndButtonView.topAnchor.constraint(equalTo: emojiAndNameView.bottomAnchor),
+            daysAndButtonView.leadingAnchor.constraint(equalTo: emojiAndNameView.leadingAnchor),
+            daysAndButtonView.trailingAnchor.constraint(equalTo: emojiAndNameView.trailingAnchor),
+            daysAndButtonView.heightAnchor.constraint(equalToConstant: 58),
+            
+            numOfDays.leadingAnchor.constraint(equalTo: daysAndButtonView.leadingAnchor, constant: 12),
+            numOfDays.topAnchor.constraint(equalTo: emojiAndNameView.bottomAnchor, constant: 16),
+            numOfDays.widthAnchor.constraint(equalToConstant: 101),
+            numOfDays.centerYAnchor.constraint(equalTo: completeTrackerButton.centerYAnchor),
+            
+            //completeTrackerButton.centerYAnchor.constraint(equalTo: numOfDays.centerYAnchor),
+            completeTrackerButton.topAnchor.constraint(equalTo: emojiAndNameView.bottomAnchor, constant: 8),
+            completeTrackerButton.heightAnchor.constraint(equalToConstant: 44),
+            completeTrackerButton.widthAnchor.constraint(equalToConstant: 44),
+            completeTrackerButton.trailingAnchor.constraint(equalTo: daysAndButtonView.trailingAnchor, constant: -12),
         ])
+    }
+    
+    @objc
+    private func didTapCompleteButton() {
+        checkedButton = !checkedButton
+        
+        if checkedButton {
+            completeTrackerButton.setTitle("", for: .normal)
+            completeTrackerButton.setImage(UIImage(named: "Done"), for: .normal)
+            completeTrackerButton.backgroundColor = UIColor(red: 51.0/255.0, green: 207.0/255.0, blue: 105.0/255.0, alpha: 0.3)
+        } else {
+            completeTrackerButton.setImage(UIImage(), for: .normal)
+            completeTrackerButton.setTitle("+", for: .normal)
+            completeTrackerButton.backgroundColor = UIColor(red: 51.0/255.0, green: 207.0/255.0, blue: 105.0/255.0, alpha: 1)
+        }
     }
     
     required init?(coder: NSCoder) {
--- Tracker/Trackers/TrackersViewController.swift
+++ Tracker/Trackers/TrackersViewController.swift
@@ -15,6 +15,18 @@ final class TrackersViewController: UIViewController {
     private var searchBar = UISearchBar()
     private let errorImageView = UIImageView()
     private let errorLabel = UILabel()
+    private let searchController = UISearchController()
+    
+    var categories = [TrackerCategory]()
+    var completedTrackers = [TrackerRecord]()
+    
+    private let emojies = [
+        "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒",
+        "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️",
+        "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄",
+    ]
+    
+    private var visibleEmojies: [String] = []
     
     private let collectionView: UICollectionView = {
         let collectionView = UICollectionView(
@@ -25,24 +37,36 @@ final class TrackersViewController: UIViewController {
         return collectionView
     }()
     
-    var categories = [TrackerCategory]()
-    var completedTrackers = [TrackerRecord]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavBar()
         setupView()
+        
+        //-------------- Example ------------------
+        let tracker1 = Tracker(id: 0, name: "Поливать растения", color: UIColor(red: 51.0/255.0, green: 207.0/255.0, blue: 105.0/255.0, alpha: 1), emoji: "🍇", schedule: ["Monday", "Friday"])
+        let tracker2 = Tracker(id: 1, name: "Читать книгу", color: UIColor(red: 255.0/255.0, green: 102.0/255.0, blue: 102.0/255.0, alpha: 1), emoji: "📚", schedule: ["Wednesday"])
+        let tracker3 = Tracker(id: 2, name: "Медитация", color: UIColor(red: 0.0/255.0, green: 150.0/255.0, blue: 255.0/255.0, alpha: 1), emoji: "🧘‍♂️", schedule: ["Everyday"])
+        let tracker4 = Tracker(id: 3, name: "Занятия спортом", color: UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 0.0/255.0, alpha: 1), emoji: "🏋️‍♂️", schedule: ["Tuesday", "Thursday"])
+        let tracker5 = Tracker(id: 4, name: "Уборка", color: UIColor(red: 75.0/255.0, green: 0.0/255.0, blue: 130.0/255.0, alpha: 1), emoji: "🧹", schedule: ["Saturday"])
+        
+        let category1 = TrackerCategory(title: "Домашний уют", trackers: [tracker1, tracker5])
+        let category2 = TrackerCategory(title: "Здоровый образ жизни", trackers: [tracker2, tracker3, tracker4])
+        
+        categories = [category1, category2]
+        //-------------- Example ------------------
+        
+        setupCollectionView()
     }
     
     private func setupNavBar() {
-        let datePicker = UIDatePicker()
+        //Date picker
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .compact
         datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: datePicker)
         
-        var addTrackerButton = UIButton()
+        //Add tracker button
         addTrackerButton = UIButton.systemButton(
             with: UIImage(named: "add_tracker_button") ?? UIImage(),
             target: self,
@@ -50,67 +74,59 @@ final class TrackersViewController: UIViewController {
         )
         addTrackerButton.tintColor = UIColor(red: 26.0/255.0, green: 27.0/255.0, blue: 34.0/255.0, alpha: 1)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: addTrackerButton)
+        
+        navigationItem.title = "Трекеры"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        searchController.searchBar.placeholder = "Поиск"
+        searchController.searchBar.tintColor = UIColor(red: 118.0/255.0, green: 118.0/255.0, blue: 128.0/255.0, alpha: 0.12)
+        searchController.searchBar.layer.cornerRadius = 30
+        searchController.searchBar.backgroundImage = UIImage()
+        
+        
+        navigationItem.searchController = searchController
+    }
+    
+    @objc
+    private func addNextEmoji() {
+        guard visibleEmojies.count < emojies.count else { return }
+
+        let nextEmojiIndex = visibleEmojies.count
+        visibleEmojies.append(emojies[nextEmojiIndex])
+        collectionView.performBatchUpdates {
+            collectionView.insertItems(at: [IndexPath(item: nextEmojiIndex, section: 0)])
+        }
+    }
+
+    @objc
+    private func removeLastEmoji() {
+        guard visibleEmojies.count > 0 else { return }
+
+        let lastEmojiIndex = visibleEmojies.count - 1
+        visibleEmojies.removeLast()
+        collectionView.performBatchUpdates {
+            collectionView.deleteItems(at: [IndexPath(item: lastEmojiIndex, section: 0)])
+        }
     }
     
     private func setupCollectionView() {
-        //TODO: setup
+        //TODO: setup collection view
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+        ])
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     private func setupView() {
         view.backgroundColor = .white
-        
-        //Title label
-        pageTitle.text = "Трекеры"
-        pageTitle.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        pageTitle.tintColor = UIColor(red: 26.0/255.0, green: 27.0/255.0, blue: 34.0/255.0, alpha: 1)
-        
-        pageTitle.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(pageTitle)
-        
-        NSLayoutConstraint.activate([
-            pageTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            pageTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
-        ])
-        
-        //Search Bar
-        searchBar.placeholder = "Поиск"
-        searchBar.tintColor = UIColor(red: 118.0/255.0, green: 118.0/255.0, blue: 128.0/255.0, alpha: 0.12)
-        searchBar.layer.cornerRadius = 30
-        searchBar.backgroundImage = UIImage()
-        
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(searchBar)
-        
-        NSLayoutConstraint.activate([
-            searchBar.heightAnchor.constraint(equalToConstant: 36),
-            searchBar.leadingAnchor.constraint(equalTo: pageTitle.leadingAnchor),
-            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            searchBar.topAnchor.constraint(equalTo: pageTitle.bottomAnchor, constant: 7)
-        ])
-        
-        //Error image
-        errorImageView.image = UIImage(named: "error")
-        errorImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(errorImageView)
-        
-        NSLayoutConstraint.activate([
-            errorImageView.widthAnchor.constraint(equalToConstant: 80),
-            errorImageView.heightAnchor.constraint(equalToConstant: 80),
-            errorImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            errorImageView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 220)
-        ])
-        
-        errorLabel.text = "Что будем отслеживать?"
-        errorLabel.textColor = UIColor(red: 26.0/255.0, green: 27.0/255.0, blue: 34.0/255.0, alpha: 1)
-        errorLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-        errorLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(errorLabel)
-        
-        NSLayoutConstraint.activate([
-            errorLabel.topAnchor.constraint(equalTo: errorImageView.bottomAnchor, constant: 8),
-            errorLabel.centerXAnchor.constraint(equalTo: errorImageView.centerXAnchor)
-        ])
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -147,3 +163,73 @@ final class TrackersViewController: UIViewController {
         print("Выбранная дата: \(formattedDate)")
     }
 }
+
+extension TrackersViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! TrackerCollectionViewCell
+        
+        cell.emoji.text = categories[indexPath.row].trackers[0].emoji
+        cell.trackerName.text = categories[indexPath.row].trackers[0].name
+        return cell
+    }
+}
+
+extension TrackersViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+}
+
+extension TrackersViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let totalSpacing: CGFloat = 9
+        let width = (collectionView.bounds.width - totalSpacing) / 2
+        return CGSize(width: width, height: 148)
+        //return CGSize(width: collectionView.bounds.width / 2, height: 148)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 9
+    }
+}
+
+
+//        //Error image
+//        errorImageView.image = UIImage(named: "error")
+//        errorImageView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(errorImageView)
+//
+//        NSLayoutConstraint.activate([
+//            errorImageView.widthAnchor.constraint(equalToConstant: 80),
+//            errorImageView.heightAnchor.constraint(equalToConstant: 80),
+//            errorImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+//            errorImageView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 220)
+//        ])
+//
+//        errorLabel.text = "Что будем отслеживать?"
+//        errorLabel.textColor = UIColor(red: 26.0/255.0, green: 27.0/255.0, blue: 34.0/255.0, alpha: 1)
+//        errorLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+//        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(errorLabel)
+//
+//        NSLayoutConstraint.activate([
+//            errorLabel.topAnchor.constraint(equalTo: errorImageView.bottomAnchor, constant: 8),
+//            errorLabel.centerXAnchor.constraint(equalTo: errorImageView.centerXAnchor)
+//        ])
